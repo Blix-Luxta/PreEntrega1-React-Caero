e7d6d365e4c6c960051061ce317450f77255cd9e
Nahuel Caero <blix.aux@gmail.com>
2023-03-10 00:04:44 -0300

    agregando contador

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,11 +1,16 @@
 import React from "react";
 import carrito from "../components/imagenes/carrito.png"
-
+import CartWidgetStyles from "./Cartwidget.css"
 
 function CartWidget() {
+
+
+
     return (
+
         <a className="nav-link position-absolute top-0 end-0" href="#">
             <img src={carrito} width="44" height="40" />
+            <span className="contador">1</span>
         </a>
     )
 }
