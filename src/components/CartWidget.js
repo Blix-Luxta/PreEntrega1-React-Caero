d3a65004cb5f61e883681d8ce205903684255275
Nahuel Caero <blix.aux@gmail.com>
2023-03-09 23:01:57 -0300

    primer commit

---
+++ src/components/CartWidget.js
@@ -0,0 +1,13 @@
+import React from "react";
+import carrito from "../components/imagenes/carrito.png"
+
+
+export const CartWidget = () => {
+    return (
+        <a className="nav-link position-absolute top-0 end-0" href="#">
+            <img src={carrito} width="44" height="40" />
+        </a>
+    )
+}
+
+export default CartWidget
---
+++ src/components/NavBar.js
@@ -0,0 +1,32 @@
+import React from "react"
+import CartWidget from "./CartWidget"
+import logo from "../components/imagenes/logo-zapa.png"
+
+
+export const NavBar = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">
+                        <img src={logo} alt="Logo" width="30" height="24" class="d-inline-block align-text-top me-2" />
+                        FootLoose</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <a className="nav-link" href="#">Features</a>
+                            <a className="nav-link" href="#">Pricing</a>
+                            <CartWidget />
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
+    )
+}
+
+export default NavBar
